77025d2bb9bfcef13d3724c1e5298b4075147193
wenwu <2321385639@qq.com>
2022-06-09 10:17:02 +0800

    hot-fix first commit

--- src/main/java/com/at/guigu/GitTest.java
+++ src/main/java/com/at/guigu/GitTest.java
@@ -2,7 +2,7 @@ package com.at.guigu;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git4");
         System.out.println("hello git");
         System.out.println("hellod ");
         System.out.println("wath>");
