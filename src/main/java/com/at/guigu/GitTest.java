1bd0fbae6a4d41492b6fa9ac42c40e7188a1990a
wenwu <2321385639@qq.com>
2022-06-09 10:20:57 +0800

    hot-fix second commit

--- src/main/java/com/at/guigu/GitTest.java
+++ src/main/java/com/at/guigu/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hellod ");
         System.out.println("wath>");
+        System.out.println("hot fix test>");
 
     }
 }
